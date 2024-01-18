11413c174f1e41e740b052a936b9adbae7da2702
xxxSevasion <sevakstepanan4@gmail.com>
2024-01-18 14:26:50 +0300

    f

--- task_2/index.php
+++ task_2/index.php
@@ -1,16 +1,5 @@
 <?php
 
-$college = array(
-    "Студенты" => array("fff", "ggg"),
-    "Преподователи" => array("fffh", "gggh"));
 
-    foreach($college as $people => $items) {
-        echo "<h1>$people</h1>";
-        echo "<ul>";
-            foreach($items as $item => $value) {
-                echo "<li>$value</li>";
-            }
-        echo "</ul>";
-    }
     
 
