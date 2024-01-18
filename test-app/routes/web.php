35cf00b7ea1e89f0d57a9e10e50f4b89e284bd54
xxxSevasion <sevakstepanan4@gmail.com>
2024-01-18 17:38:27 +0300

    Развернули проект на ларавель, добавили файл about.blade.php и написали свой route

---
+++ test-app/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// Открытие главной страницы
+Route::get('/', function () {
+    return view('welcome');
+});
+// Открытие страницы о нас
+Route::get('/about', function () {
+   return view('about');
+});
